336c72241bdade2d5c2936059b438d439e7f978b
alexrmn <roman.alex.93@gmail.com>
2022-12-16 11:12:58 +0200

    added Ingredient and UnitOfMeasure entities and mapped them.

---
+++ src/main/java/com/recipies/springrecipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,15 @@
+package com.recipies.springrecipeapp.domain;
+
+public class UnitOfMeasure {
+
+    private String uom;
+
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+}
